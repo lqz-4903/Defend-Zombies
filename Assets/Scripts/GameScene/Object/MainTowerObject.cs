58e90ac45390e9e1671414cadbc6cd7cc99805da
lqz-4903 <3101479332@qq.com>
2026-04-19 23:14:03 +0800

    完成了玩家信息，保护区信息

---
+++ Assets/Scripts/GameScene/Object/MainTowerObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTowerObject : MonoBehaviour
+{
+    //血量相关
+    private int hp;
+    private int maxHP;
+    //是否死亡
+    private bool isDead = false;
+
+    //能够被别人快速获取到位置
+    private static MainTowerObject instance;
+    public static MainTowerObject Instance => instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //更新血量
+    public void UpdateHP(int hp, int maxHP)
+    {
+        this.hp = hp;
+        this.maxHP = maxHP;
+
+        //更新界面上的血量的显示
+        UIManager.Instance.GetPanel<GamePanel>().UpdataTowerHP(hp, maxHP);
+    }
+
+    //自己受到伤害
+    public void Wound(int dmg)
+    {
+        //如果保护区死了 没必要再减
+        if (isDead)
+            return;
+        //受到伤害
+        hp -= dmg;
+        //死亡逻辑
+        if (hp <= 0)
+        {
+            hp = 0;
+            isDead = true;
+            //游戏结束
+        }
+        //更新血量
+        UpdateHP(hp, maxHP);
+    }
+
+    private void OnDestroy()
+    {
+        instance = null;
+    }
+}
---
+++ Assets/Scripts/GameScene/Object/PlayerObject.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+{
+    private Animator animator;
+
+    //1.玩家属性的初始化
+    //玩家攻击力
+    private int atk;
+    //玩家拥有的钱
+    public int money;
+    //旋转的速度
+    private float roundSpeed = 50;
+
+    //跟随的摄像机
+    private CameraMove cameraMove;
+
+    //持枪对象才有的开火点
+    public Transform gunPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        cameraMove = GameObject.Find("_Cinematic_camera1").GetComponent<CameraMove>();
+        cameraMove.SetTarget(this.transform);
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //2.移动变化 动作变化
+        //移动动作的变换 由于动作有位移 我们也应用了动作的位移 所以只要改变这两个值 就会有动作的变化 和 速度的变化
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // 按住Shift时加速奔跑
+        float speedMultiplier = Input.GetKey(KeyCode.LeftShift) ? 1f : 0.5f;
+
+        animator.SetFloat("VSpeed", vertical * speedMultiplier);
+        animator.SetFloat("HSpeed", horizontal * speedMultiplier);
+
+        //旋转
+        this.transform.Rotate(Vector3.up, roundSpeed * Input.GetAxis("Mouse X") * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            animator.SetTrigger("Roll");
+
+        if (Input.GetMouseButton(0))
+            animator.SetTrigger("Fire");
+
+    }
+
+    /// <summary>
+    /// 初始化玩家属性
+    /// </summary>
+    /// <param name="atk"></param>
+    /// <param name="money"></param>
+    public void InitPlayerInfo(int atk, int money)
+    {
+        this.atk = atk;
+        this.money = money;
+
+        //更新界面上钱的数量
+        UpdateMoney();
+    }
+
+    //3.攻击动作的不同处理
+    /// <summary>
+    /// 专门用于处理刀类武器攻击动作的伤害检测事件
+    /// </summary>
+    public void KnifeEvent()
+    {
+        //进行伤害检测
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position + this.transform.forward + this.transform.up, 1,
+                                                     1 << LayerMask.NameToLayer("Monster"));
+
+        
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            //得到碰到撞到的对象上的怪物脚本 让其受伤
+        }
+    }
+
+
+    public void ShootEvent()
+    {
+        //进行射线检测
+        //前提是需要开户点
+        RaycastHit[] hits = Physics.RaycastAll(new Ray(gunPoint.position, gunPoint.forward), 1000,
+                                                1 << LayerMask.NameToLayer("Monster"));
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            //得到对象上的怪物脚本 让其受伤
+        }
+    }
+
+
+    //4.钱变化的逻辑    
+    public void UpdateMoney()
+    {
+        //间接地更新界面上 钱的数量
+        UIManager.Instance.GetPanel<GamePanel>().UpdateMoney(money);
+    }
+
+    /// <summary>
+    /// 提供给外部加钱的方法
+    /// </summary>
+    /// <param name="money"></param>
+    public void AddMoney(int money)
+    {
+        //加钱
+        this.money += money;
+        UpdateMoney();
+    }
+}
